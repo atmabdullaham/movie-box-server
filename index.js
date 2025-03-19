9ec06c59b2f97cdaa5483c0323e57b3811780f6a
ATM Abdullah Al Mamun <atmabdullaham@gmail.com>
2025-03-19 11:17:33 +0600

    - Created /movie endpoint to handle POST requests - Integrated MongoDB for storing movie details - Implemented data validation and error handling - Enabled CORS to allow frontend communication - Implement dotenv

--- index.js
+++ index.js
@@ -1,12 +1,54 @@
 const express = require('express');
 const cors = require('cors');
+require('dotenv').config()
 const app = express()
 const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+
 
 
 // middleware
 app.use(cors())
 app.use(express.json())
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.4lxln.mongodb.net/?appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+ serverApi: {
+  version: ServerApiVersion.v1,
+  strict: true,
+  deprecationErrors: true,
+ }
+});
+
+async function run() {
+ try {
+  // Connect the client to the server	(optional starting in v4.7)
+  await client.connect();
+  const database = client.db("movieDB");
+  const movieCollection = database.collection("movies");
+
+  app.post("/movie", async (req, res) => {
+   const newMovie = req.body
+   console.log(newMovie)
+   const result = await movieCollection.insertOne(newMovie);
+   res.send(result)
+
+  })
+
+  // Send a ping to confirm a successful connection
+  await client.db("admin").command({ ping: 1 });
+  console.log("Pinged your deployment. You successfully connected to MongoDB!");
+ } finally {
+  // Ensures that the client will close when you finish/error
+  // await client.close();
+ }
+}
+run().catch(console.dir);
 
 
 app.get("/", (req, res) => {
